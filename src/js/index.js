8e8e5c786ddd11e33682119a916856d7f63d92d6
Oluwadare Seyi <oluwadareseyii@gmail.com>
2025-03-17 14:21:41 +0000

    feat: build generator

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
+import { getVideoPoster } from "./utils/cloudinary";
 
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+const resultImage = document.querySelector("[data-result-image]");
+const downloadButton = document.querySelector("[data-download]");
+const copyButton = document.querySelector("[data-copy]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const url = formData.get("url");
+  const time = formData.get("time");
+
+  if (!url) {
+    // alert("Please enter a valid URL");
+    return;
+  }
+
+  const poster = getVideoPoster(url, time);
+  resultImage.src = poster;
+});
+
+downloadButton.addEventListener("click", () => {
+  const poster = resultImage.src;
+  // fetch the image
+  fetch(poster)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "thumbnail.jpg";
+      a.click();
+    });
+});
+
+copyButton.addEventListener("click", () => {
+  const poster = resultImage.src;
+  navigator.clipboard.writeText(poster);
+});
--- src/js/utils/cloudinary.js
+++ src/js/utils/cloudinary.js
@@ -1,11 +1,25 @@
-export const getVideoPoster = (videoUrl) => {
-  const videoToImg = videoUrl.replace(".mp4", ".jpg");
+const videoExtensions = [
+  ".mp4",
+  ".mov",
+  ".avi",
+  ".wmv",
+  ".flv",
+  ".mpeg",
+  ".mpg",
+  ".m4v",
+  ".webm",
+  ".mkv",
+];
 
-  const posterImage = videoToImg.replace("upload", "upload/q_auto,f_auto,so_0");
+export const getVideoPoster = (videoUrl, time = 0) => {
+  // Create regex pattern from video extensions array
+  const videoExtPattern = new RegExp(videoExtensions.join("|"), "i");
+  // Replace any matching video extension with .jpg
+  const videoToImg = videoUrl.replace(videoExtPattern, ".jpg");
 
+  const posterImage = videoToImg.replace(
+    "upload",
+    `upload/q_auto,f_auto,so_${time}`
+  );
   return posterImage;
-};
-
-export const optimizeCloudinaryUrl = (url) => {
-  return url.replace("upload", "upload/q_auto,f_auto");
 };
